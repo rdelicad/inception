a5ae98aaab06410df6051910389aa5d9a49d630f
rdelicad <rdelicad@gmail.com>
2024-08-11 19:09:20 +0000

    configure wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,19 +1,32 @@
 <?php
+// Database configuration
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_USER_PASS'));
 define('DB_HOST', 'mariadb:3306');
 
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+// WordPress URL and title configuration
+define('WP_HOME', 'https://' . getenv('WP_URL'));
+define('WP_SITEURL', 'https://' . getenv('WP_URL'));
 
+// Security keys generation
+define('AUTH_KEY',         '*>Df,Av]}Kc`5jy--d.-^!V3>|4#hX$tW7n(3Q,]* d0|}h!]j*H+RF)sT@?OThr');
+define('SECURE_AUTH_KEY',  '$fKEak{a7/2#/fkc3N+2yC6-1$f8z* ]!=baQ<(W:RE5ijO+ W^WSZWjw,:vAfSv');
+define('LOGGED_IN_KEY',    '04DY> VG<H?qWH?>ti ,vS8B:{-5Q3]Gc+iAhi42kv_!<ebBtw+740-q8Hx`dA-R');
+define('NONCE_KEY',        'Skp.GTnn)8VevmKel:T;SBJ49byf[yq-EN,i^]b0_,W{c~-nDPo$xb+JL-`Jpt{s');
+define('AUTH_SALT',        'dOr/O>i*&QP0-D8D57WoKfpD0-iP*5^>jP%v-5~nlJ8x205vU#[WeuofW9K1pW|h');
+define('SECURE_AUTH_SALT', ':0V}L)33yYHT |[U1[r}BZu9eR4JTo7r/)wR-*]H%}c!nQ/`$ijqC-u)C}R#-arz');
+define('LOGGED_IN_SALT',   'LgG!7yp&kJ@gXU|,O6|4=pcT!!{T7ronBCx@4IHc3Kqi[mcdi^@I`NyJ7c1@|(9E');
+define('NONCE_SALT',       ' [P%U}8&I+bfKgTo7]UR)`TSI3I2;=yT96@W,<$rEh#$(n?-`qrU-W/w[,,.eiNA');
+
+// WordPress database table prefix
 $table_prefix = 'wp_';
 
+// Debug mode
 define('WP_DEBUG', false);
+
+// Absolute path to the WordPress directory and WordPress setup
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
 
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set permissions
-chown -R www-data:www-data /var/www/html
-chmod 755 /var/www/html
+set -e
 
-# Start PHP-FPM
-echo "Starting PHP-FPM"
-exec php-fpm7.4 -F
+# waiting for the database to be ready
+until mysql -hmariadb -uroot -p${DB_ROOT_PASS} -e "SHOW DATABASES;" > /dev/null 2>&1; do
+  echo "waiting for mariadb to be available..."
+  sleep 3
+done
+
+# Configure Wordpress users
+wp core install --url=${WP_URL} --title=${WP_TITLE} \
+    --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} \
+    --admin_email=${WP_ADMIN_EMAIL} --skip-email --path=/var/www/html
+
+wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASS} --role=subscriber --path=/var/www/html
+
+# Init PHP-FPM
+exec php-fpm7.4 --nodaemonize
 

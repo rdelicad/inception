ca66f3f67cce0bddb5193e6b4926ca216844984f
rdelicad <rdelicad@gmail.com>
2024-08-12 19:44:50 +0000

    mariadb guay

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,27 +1,19 @@
 #!/bin/sh
 
-# Crear los directorios necesarios y ajustar permisos
 mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+chown -R mysql:root /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-# Esperar a que MariaDB esté disponible
-echo "Esperando a que MariaDB esté listo..."
-while ! mysql -uroot -p"$DB_ROOT_PASS" -e "SELECT 1" >/dev/null 2>&1; do
-  >&2 echo "MariaDB no está disponible - esperando..."
-  sleep 3
-done
+cat << EOF > /var/lib/mysql/init.sql
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
 
-# Inicializar la base de datos
-echo "Inicializando la base de datos..."
-mysql -uroot -p"$DB_ROOT_PASS" <<-EOSQL
-  CREATE DATABASE IF NOT EXISTS $DB_NAME;
-  CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS';
-  GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-  FLUSH PRIVILEGES;
-EOSQL
+chmod 644 /var/lib/mysql/init.sql
 
-# Iniciar MariaDB
-echo "Iniciando MariaDB..."
-exec mysqld --bind-address=0.0.0.0
+echo "Starting MariaDB..."
+exec mysqld --init-file=/var/lib/mysql/init.sql --bind-address=0.0.0.0
 
